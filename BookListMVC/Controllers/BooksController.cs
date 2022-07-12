a75656ff1488a95b9b088f3b9d135460d27a3782
frankz168 <franky.sutanto93@gmail.com>
2022-07-12 22:46:54 +0700

    first commit

---
+++ BookListMVC/Controllers/BooksController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListMVC.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public BooksController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Book Book { get; set; }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            Book = new Book();
+            if (id == null)
+                //create
+                return View(Book);
+            //update
+            Book = _db.Book.FirstOrDefault(m => m.Id == id);
+            if (Book == null)
+                return NotFound();
+
+            return View(Book);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert()
+        {
+            if (ModelState.IsValid)
+            {
+                if (Book.Id == 0)
+                    // insert query in DB
+                    _db.Book.Add(Book);
+                else
+                    // update query in DB
+                    _db.Book.Update(Book);
+
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(Book);
+        }
+
+
+        #region API Calls
+        [HttpGet]
+        public async Task<IActionResult> GetBookAll()
+        {
+            var test = await _db.Book.ToListAsync();
+            return Json(new { data = await _db.Book.ToListAsync() });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var bookFromDB = await _db.Book.FirstOrDefaultAsync(u => u.Id == id);
+            if (bookFromDB == null)
+                return Json(new { success = false, message = "Error while Deleting" });
+
+            _db.Book.Remove(bookFromDB);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Delete successfully" });
+        }
+
+        #endregion
+    }
+}
---
+++ BookListMVC/wwwroot/js/bookList.js
@@ -0,0 +1,69 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#datatableBook').DataTable({
+        "ajax": {
+            "url": "/books/getbookall/",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "name", "width": "20%" },
+            { "data": "author", "width": "20%" },
+            { "data": "isbn", "width": "20%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="text-center">
+                        <a href="/Books/Upsert?id=${data}" class='btn btn-success text-white' style='cursor:pointer; width:70px;'>
+                            Edit
+                        </a>
+                        &nbsp;
+                        <a class='btn btn-danger text-white' style='cursor:pointer; width:70px;'
+                            onclick=Delete('/books/Delete?id='+${data})>
+                            Delete
+                        </a>
+                        </div>`;
+                }, "width": "40%"
+            }
+        ],
+        "language": {
+            "emptyTable": "no data found"
+        },
+        "width": "100%"
+    });
+}
+
+function Delete(url) {
+    swal({
+        title: "Are you sure ?",
+        text: "Once deleted, you will not able to recover",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        swal("Deleted! Your file has been deleted", data.d, "success");
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                      /*  $('#datatableBook').DataTable().draw();*/
+                    }
+                    else {
+                        toastr.error(message);
+                    }
+                }
+            });
+        }
+    });
+}
